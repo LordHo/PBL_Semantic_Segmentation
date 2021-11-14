a9df716f7fa1cb69981b0793e35ca461a69a1f10
Philip-Ho <philiphoyunju@gmail.com>
2021-11-14 13:33:37 +0800

    Append DeepLabv3+

--- refactor-0821/loss.py
+++ refactor-0821/loss.py
@@ -9,6 +9,7 @@ from UNet_Version.loss.iouLoss import IOU_loss, classes_IOU_loss, weighted_class
 from UNet_Version.loss.focalLoss import separate_focal_loss
 from UNet_Version.loss.diceLoss import separate_dice_loss
 
+
 class Loss:
     def __init__(self):
         self.resetall()
@@ -26,17 +27,17 @@ class Loss:
 
     def setting(self):
         colors = {
-                  'nothing': 0.0, # gray: 0.5, setting to 0, 0.5 -> 0
-            'img_whiteside': 1.0, # white: 1.0
-               'background': 1.0, # black: 1.0
-               'bacmixgums': 1.0, # brown: 1.5, this need to be lower, 1.5 -> 1.0
-          'artifical_crown': 1.0, # fluorescent blue: 1.5, this need to be lower, 1.5 -> 1.0
-                    'tooth': 2.5, # yellow: 2
-                  'overlap': 1.0, # pink: 4
-                   'cavity': 2.5, # red: 2
-                      'cej': 2.5, # green: 2
-                     'gums': 2.5, # blue: 2
-            'img_depressed': 2.5, # orange: 1.5, this need to be lower, 3 -> 1.5
+            'nothing': 0.0,  # gray: 0.5, setting to 0, 0.5 -> 0
+            'img_whiteside': 1.0,  # white: 1.0
+            'background': 1.0,  # black: 1.0
+            'bacmixgums': 1.0,  # brown: 1.5, this need to be lower, 1.5 -> 1.0
+            'artifical_crown': 1.0,  # fluorescent blue: 1.5, this need to be lower, 1.5 -> 1.0
+            'tooth': 2.5,  # yellow: 2
+            'overlap': 1.0,  # pink: 4
+            'cavity': 2.5,  # red: 2
+            'cej': 2.5,  # green: 2
+            'gums': 2.5,  # blue: 2
+            'img_depressed': 2.5,  # orange: 1.5, this need to be lower, 3 -> 1.5
         }
         # self.dice_weights = np.array(list(colors.values()))
         # JingPing
@@ -46,15 +47,15 @@ class Loss:
         # self.dice_weights = np.ones(11)
         # self.dice_weights[0] = 0
         self.useBCE = False
-        self.useFocal = False
-        self.useDice = False
+        self.useFocal = True
+        self.useDice = True
         self.useIoU = True
         self.useClassesIoU = False
         self.useWeightedClassesIoU = False
 
         self.focal_loss_function = smp.losses.FocalLoss(mode='multiclass')
-        self.dice_loss_function = smp.losses.DiceLoss(mode='multiclass', classes=self.dice_weights)
-
+        self.dice_loss_function = smp.losses.DiceLoss(
+            mode='multiclass', classes=self.dice_weights)
 
         self.useCE = True
         self.cross_entropy = nn.CrossEntropyLoss()
@@ -98,10 +99,10 @@ class Loss:
         if self.useClassesIoU:
             self.calculate_classes_iou_loss(output, y)
         if self.useWeightedClassesIoU:
-                self.calculate_weighted_classes_iou_loss(output, y)
+            self.calculate_weighted_classes_iou_loss(output, y)
         if self.useCE:
             self.calculate_ce_loss(output, y)
-        
+
         self.sum_loss()
 
         return self.seg_loss
@@ -111,21 +112,22 @@ class Loss:
 
     def calculate_focal_loss(self, output, y_):
         self.focal_loss = self.focal_loss_function(output, y_)
-    
+
     def calculate_separate_focal_loss(self, output, y):
         self.focal_loss = separate_focal_loss(output, y)
 
     def calculate_dice_loss(self, output, y_):
         self.dice_loss = self.dice_loss_function(output, y_)
-    
+
     def calculate_separate_dice_loss(self, output, y):
-        self.dice_loss = separate_dice_loss(output, y, class_weights=self.dice_weights)
-    
+        self.dice_loss = separate_dice_loss(
+            output, y, class_weights=self.dice_weights)
+
     def calculate_iou_loss(self, output, y):
         self.iou_loss = IOU_loss(output, y)
         # due to no use IoU but the grad will still occupy GPU memory and cause run out of memory
         self.iou_loss = self.iou_loss.detach()
-    
+
     def calculate_classes_iou_loss(self, output, y):
         self.classes_iou_loss = classes_IOU_loss(output, y)
 
@@ -134,7 +136,7 @@ class Loss:
 
     def calculate_ce_loss(self, output, y):
         self.ce_loss = self.cross_entropy(output, y)
-    
+
     def print_used_losses(self):
         losses = []
         if self.useBCE:
@@ -156,6 +158,6 @@ class Loss:
 
     def print_dice_weights(self):
         return ', '.join(self.dice_weights.astype(str))
-    
+
     def print_WCIoU_weights(self):
         return ', '.join(print_WCIoU_weights().astype(str))
--- refactor-0821/main.py
+++ refactor-0821/main.py
@@ -7,6 +7,11 @@ from train import training
 from test import testing
 from predict import predicting
 
+
+def main_log(message, file):
+    file.write(message+'\n')
+
+
 if __name__ == '__main__':
     mode = Mode.TRAIN
     image_mode = ImageMode.RGB
@@ -22,29 +27,30 @@ if __name__ == '__main__':
     result_dir = os.path.join('..', 'result', timestamp)
     createDir(result_dir)
 
+    mainLog_file = open(os.path.join(result_dir, 'main log.txt'), 'w')
+
     train_root_dir = os.path.join('..', 'dataset', 'modify_single_train')
-    # image_dir = os.path.join('..', 'dataset', '14X label', 'can add into labels ( stage 3 in it)', 'selection')
-    # image_dir = os.path.join('..', 'dataset', 'pre_train', 'images')
-    # image_dir = os.path.join('..', 'dataset', 'root_and_image_data', 'images')
-    # image_dir =  os.path.join('..', '..', 'segmentation', 'image')
     image_dir = os.path.join(train_root_dir, 'images')
     print(f'Image Dir: {image_dir}')
+    main_log(f'Image Dir: {image_dir}', mainLog_file)
 
     if mode == Mode.TRAIN:
         k = 3
-        # label_dir = os.path.join('..', 'dataset', 'pre_train', 'labels')
         label_dir = os.path.join(train_root_dir, 'labels')
         print(f'Label Dir: {label_dir}')
+        main_log(f'Label Dir: {label_dir}', mainLog_file)
 
         valid = True if Dataset.VALID in datasets else False
         test = True if Dataset.TEST in datasets else False
         " test only when single dir can remove, if k fold will at least a pratition be testing data "
         if not use_existes_partition:
-            split_data(mode, result_dir, image_dir, label_dir=label_dir, k=k, valid=valid, test=test)
+            split_data(mode, result_dir, image_dir,
+                       label_dir=label_dir, k=k, valid=valid, test=test)
         else:
-            # partitions_dir = os.path.join('..', 'special partitions', 'Single_Row_132_image')
-            partitions_dir = os.path.join('..', 'special partitions', 'Single_Patient_Single_Row_132_image')
+            partitions_dir = os.path.join(
+                '..', 'special partitions', 'Single_Patient_Single_Row_132_image')
             print(f'Special Paritions: {partitions_dir}')
+            main_log(f'Special Paritions: {partitions_dir}', mainLog_file)
             partition_counts = copy_partitions(partitions_dir, result_dir)
             if partition_counts == 0:
                 raise "Partition Error!"
@@ -52,28 +58,47 @@ if __name__ == '__main__':
                 k = partition_counts
             only_assign_k_fold(k, result_dir, valid=valid)
         print(f'Total Folder Number: {k}')
-    
-        train_pars = {
-            'n_classes'     : 10 + 1,
-            # 'model_name'    : 'UNet', 
-            'model_name'    : 'PSPNet',
-            # 'backbone'      : 'efficientnet-b3',
-            'backbone'      : 'resnet34',
-            'activation'    : 'softmax2d',
-            'image_mode'    : image_mode,
-        }
-        # image_mode options are 'rgb', 'gray'
+        main_log(f'Total Folder Number: {k}', mainLog_file)
 
+        # U-Net Model
+        # train_pars = {
+        #     'n_classes'     : 10 + 1,
+        #     'model_name'    : 'UNet',
+        #     'backbone'      : 'efficientnet-b3',
+        #     'activation'    : 'softmax2d',
+        #     'image_mode'    : image_mode,
+        # }
+        # train_pars = {
+        #     'n_classes'     : 10 + 1,
+        #     'model_name'    : 'PSPNet',
+        #     'backbone'      : 'resnet152',
+        #     'activation'    : 'softmax2d',
+        #     'image_mode'    : image_mode,
+        # }
+        train_pars = {
+            'n_classes': 10 + 1,
+            'model_name': 'DeepLabv3+',
+            'backbone': 'efficientnet-b3',
+            'activation': 'softmax2d',
+            'image_mode': image_mode,
+        }
+        main_log(f'Train pars:', mainLog_file)
+        for key in train_pars.keys():
+            main_log(f'{key}: {train_pars[key]}', mainLog_file)
+        # image_mode options are 'rgb', 'gray'
+        print(train_pars)
         if k == 1 or k is None:
             model = Model(**train_pars)
             if Dataset.TRAIN in datasets:
                 train_file_path = os.path.join(result_dir, 'train.txt')
                 print(f'Training.')
-                model = training(train_file_path, image_dir, label_dir, result_dir, model, image_mode=image_mode, label_mode=label_mode)
+                model = training(train_file_path, image_dir, label_dir, result_dir,
+                                 model, image_mode=image_mode, label_mode=label_mode)
 
                 if Dataset.TEST in datasets:
                     test_file_path = os.path.join(result_dir, 'test.txt')
-                    testing(test_file_path, image_dir, result_dir, model, image_mode=image_mode, label_mode=label_mode)
+                    testing(test_file_path, image_dir, result_dir, model,
+                            image_mode=image_mode, label_mode=label_mode)
         elif k > 1:
             for i in range(1, k+1, 1):
                 model = Model(**train_pars)
@@ -82,11 +107,13 @@ if __name__ == '__main__':
                 if Dataset.TRAIN in datasets:
                     train_file_path = os.path.join(fold_dir, 'train.txt')
                     print(f'Fold {i} is training.')
-                    model = training(train_file_path, image_dir, label_dir, fold_dir, model, image_mode=image_mode, label_mode=label_mode)
+                    model = training(train_file_path, image_dir, label_dir, fold_dir,
+                                     model, image_mode=image_mode, label_mode=label_mode)
 
                     if Dataset.TEST in datasets:
                         test_file_path = os.path.join(fold_dir, 'test.txt')
-                        testing(test_file_path, image_dir, fold_dir, model, image_mode=image_mode, label_mode=label_mode)
+                        testing(test_file_path, image_dir, fold_dir, model,
+                                image_mode=image_mode, label_mode=label_mode)
         else:
             raise "k value is wrong"
 
@@ -96,14 +123,22 @@ if __name__ == '__main__':
 
         predict_file_path = os.path.join(result_dir, 'predict.txt')
         load_weight_path_list = [
-           os.path.join('..', 'result', fold_name, 'fold 1', 'save_model', 'UNet-best.pkl'),
-           os.path.join('..', 'result', fold_name, 'fold 2', 'save_model', 'UNet-best.pkl'),
-           os.path.join('..', 'result', fold_name, 'fold 3', 'save_model', 'UNet-best.pkl'),
-           os.path.join('..', 'result', fold_name, 'fold 4', 'save_model', 'UNet-best.pkl'),
-           os.path.join('..', 'result', fold_name, 'fold 5', 'save_model', 'UNet-best.pkl'),
+            os.path.join('..', 'result', fold_name, 'fold 1',
+                         'save_model', 'UNet-best.pkl'),
+            os.path.join('..', 'result', fold_name, 'fold 2',
+                         'save_model', 'UNet-best.pkl'),
+            os.path.join('..', 'result', fold_name, 'fold 3',
+                         'save_model', 'UNet-best.pkl'),
+            os.path.join('..', 'result', fold_name, 'fold 4',
+                         'save_model', 'UNet-best.pkl'),
+            os.path.join('..', 'result', fold_name, 'fold 5',
+                         'save_model', 'UNet-best.pkl'),
         ]
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        predicting(predict_file_path, image_dir, result_dir, load_weight_path_list, device, image_mode)
-    
+        predicting(predict_file_path, image_dir, result_dir,
+                   load_weight_path_list, device, image_mode)
+
     else:
         raise "Mode is wrong."
+
+    mainLog_file.close()
